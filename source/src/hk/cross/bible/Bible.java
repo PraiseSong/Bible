2149cb918b2c53d441d3fca636bc347e761c37a8
praise <qiqiyoujian@gmail.com>
2013-02-02 20:02:05 +0800

    靠，终于把速度提上来了

--- source/src/hk/cross/bible/Bible.java
+++ source/src/hk/cross/bible/Bible.java
@@ -37,14 +37,16 @@ public class Bible extends Activity {
 	
 	private JSONArray bookTitle;
 	
+	private String tome = "";//当前书卷的名称
 	private int tomeID = 0;//当前圣经书卷的id
 	private int chapterID = 0;//当前的章数
 	private int sectionFromID = 0;//当前开始节数
 	private int sectionToID = 0;//当前结束节数
 	
-	private boolean initQuery = false;//初始化查询是否完成 
-	
-	private QueryTask queryTask;
+	private QueryTome queryTome;
+	private QueryChapterNum queryChapterNum;
+	private QuerySectionNum querySectionNum;
+	private QueryBibleDetail queryBibleDetail;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +54,8 @@ public class Bible extends Activity {
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_bible);
 		
-		queryTask = new QueryTask(Bible.this);
-		queryTask.execute(0);
+		queryTome = new QueryTome(Bible.this);
+		queryTome.execute(0);
 		
 		/**
 		 * 解决android NetworkOnMainThreadException报错
@@ -70,65 +72,7 @@ public class Bible extends Activity {
 		.penaltyDeath()
 		.build());	
 	}
-	
-	/**
-	 * 处理圣经书卷
-	 */
-	private void processBookTitle(){
-		String bookTitleQuery = "action=query_bookTitle";
 		
-		try{
-			bookTitle = Json.getJSONArray(HOST+bookTitleQuery);
-			List<JSONObject> tome = jsonArray2ListJSONObject(bookTitle);
-			List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
-			Spinner tomeList = (Spinner) findViewById(R.id.tome);
-			for(int i=0; i<tome.size(); i++){
-				HashMap<String, Object> map = new HashMap<String, Object>();
-				map.put("name", tome.get(i).getString("BookTitle"));
-				map.put("id", tome.get(i).getString("Book"));
-				data.add(map);
-			}
-			SimpleAdapter _Adapter = new SimpleAdapter(Bible.this, data, R.layout.list_item, new String[] {
-					"id", "name"
-			}, new int[] {
-					R.id.id,
-					R.id.name
-			});
-			tomeList.setAdapter(_Adapter);
-			
-			OnItemSelectedListener listener = new OnItemSelectedListener(){
-				@Override
-				public void onItemSelected(AdapterView<?> parent, View view,
-						int position, long id) {
-					try {
-						JSONObject item = new JSONObject(parent.getItemAtPosition(position).toString());
-						int itemId = item.getInt("id");
-						String itemId2 = null;
-						if(itemId < 10){
-							itemId2 = "0"+String.valueOf(itemId);
-						}else{
-							itemId2 = ""+itemId;
-						}
-						
-						tomeID = Integer.parseInt(itemId2);
-						
-						processChapterNum();
-					} catch (JSONException e) {
-						e.printStackTrace();
-					}
-				}
-
-				@Override
-				public void onNothingSelected(AdapterView<?> arg0) {
-				}};
-			tomeList.setOnItemSelectedListener(listener);
-		}catch(JSONException e){
-			e.printStackTrace();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
 	/**
 	 * 将一个json array转换为List对象
 	 * @param jsonData
@@ -143,113 +87,18 @@ public class Bible extends Activity {
 		}
 		return data;
 	}
-	
-	private void processChapterNum(){
-		String chapterQuery = "action=query_article_num&id="+tomeID;
-		
-		try{
-			int chapterNum = Integer.parseInt(Json.getRequest(HOST+chapterQuery).trim());
-			Integer[] chapterNumList = new Integer[chapterNum];
-			for(int i=0; i<chapterNum; i++){
-				chapterNumList[i] = i+1;
-			}
-			Spinner chapter = (Spinner) findViewById(R.id.chapterNum);
-			ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(Bible.this, android.R.layout.simple_list_item_1, chapterNumList);
-			chapter.setAdapter(adapter);
-			
-			OnItemSelectedListener listener = new OnItemSelectedListener(){
-				@Override
-				public void onItemSelected(AdapterView<?> parent, View view,
-						int position, long id) {
-					chapterID = (int) Integer.parseInt(parent.getItemAtPosition(position).toString());
-					processSectionNum();
-				}
-
-				@Override
-				public void onNothingSelected(AdapterView<?> arg0) {
-				}};
-			chapter.setOnItemSelectedListener(listener);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	private void processSectionNum(){
-		String sectionQuery = "action=query_verse_num&article="+chapterID+"&id="+tomeID+"";
-		
-		try{
-			int sectionNum = Integer.parseInt(Json.getRequest(HOST+sectionQuery).trim());
-			Integer[] sectionList = new Integer[sectionNum];
-			for(int i=0; i<sectionNum; i++){
-				sectionList[i] = i+1;
-			}
-			Spinner sectionFrom = (Spinner) findViewById(R.id.sectionFrom);
-			Spinner sectionTo = (Spinner) findViewById(R.id.sectionTo);
-			ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(Bible.this, android.R.layout.simple_list_item_1, sectionList);
-			sectionFrom.setAdapter(adapter);
-			sectionTo.setAdapter(adapter);
-			
-			OnItemSelectedListener listenerTo = new OnItemSelectedListener(){
-				@Override
-				public void onItemSelected(AdapterView<?> parent, View view,
-						int position, long id) {
-					sectionToID = (int) Integer.parseInt(parent.getItemAtPosition(position).toString());
-					queryBible();
-				}
-
-				@Override
-				public void onNothingSelected(AdapterView<?> arg0) {
-				}};
-			OnItemSelectedListener listenerFrom = new OnItemSelectedListener(){
-					@Override
-					public void onItemSelected(AdapterView<?> parent, View view,
-							int position, long id) {
-						sectionFromID = (int) Integer.parseInt(parent.getItemAtPosition(position).toString());
-						if(initQuery){
-							queryBible();
-						}
-					}
-
-					@Override
-					public void onNothingSelected(AdapterView<?> arg0) {
-					}};
-			sectionFrom.setOnItemSelectedListener(listenerFrom);
-			sectionTo.setOnItemSelectedListener(listenerTo);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	private void queryBible(){
-		if(sectionFromID > sectionToID){
-			return;
-		}
-		
-		String queryBible = "action=query_bible&article="+chapterID+"&id="+tomeID+"&verse_start="+sectionFromID+"&verse_stop="+sectionToID+"";
-		try{
-			String bible = (Json.getRequest(HOST+queryBible)).trim();
-			bible = android.text.Html.fromHtml(bible).toString();
-			TextView bibleBox = (TextView) findViewById(R.id.bibleBox);
-			bibleBox.setText(bible);
-			
-			initQuery = true;
-			queryTask.dialog.dismiss();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	private void loading(){
-		setProgressBarIndeterminateVisibility(true);
-	}
    
-	class QueryTask extends AsyncTask<Integer, Integer, SimpleAdapter>{
+	/**
+	 * 查询所有圣经书卷
+	 * @author qizhuq
+	 */
+	class QueryTome extends AsyncTask<Integer, Integer, SimpleAdapter>{
 		private Activity activity;
 		private ProgressDialog dialog;
 		private Context context;
 
-		public QueryTask(Activity activity){
-			Log.d(TAG,"开始一个新的查询任务");
+		public QueryTome(Activity activity){
+			Log.d(TAG,"开始查询圣经书卷");
 			this.activity = activity;
 			context = activity;
 			dialog = new ProgressDialog(context);
@@ -257,8 +106,7 @@ public class Bible extends Activity {
 		
 		protected void onPreExecute(){
 			Log.d(TAG,"onPreExecute");
-			this.dialog.setTitle(TAG);
-			this.dialog.setMessage("查询中...");
+			this.dialog.setTitle("查询所有"+TAG+"书卷");
 			if(!this.dialog.isShowing()){
 				this.dialog.show();
 			}
@@ -271,6 +119,8 @@ public class Bible extends Activity {
 			Spinner tomeList = (Spinner) findViewById(R.id.tome);
 			tomeList.setAdapter(adapter);
 			
+			final ProgressDialog dialog = this.dialog;
+			
 			OnItemSelectedListener listener = new OnItemSelectedListener(){
 				@Override
 				public void onItemSelected(AdapterView<?> parent, View view,
@@ -278,6 +128,7 @@ public class Bible extends Activity {
 					try {
 						JSONObject item = new JSONObject(parent.getItemAtPosition(position).toString());
 						int itemId = item.getInt("id");
+						tome = item.getString("name");
 						String itemId2 = null;
 						if(itemId < 10){
 							itemId2 = "0"+String.valueOf(itemId);
@@ -286,8 +137,9 @@ public class Bible extends Activity {
 						}
 						
 						tomeID = Integer.parseInt(itemId2);
+						dialog.dismiss();
 						
-						processChapterNum();
+						queryChapterNum = (QueryChapterNum) new QueryChapterNum(Bible.this).execute(0);
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
@@ -334,6 +186,203 @@ public class Bible extends Activity {
 				e.printStackTrace();
 			}
 			return null;
+		}
+	}
+    
+	/**
+	 * 查询当前书卷下的所有章数
+	 * @author qizhuq
+	 */
+	class QueryChapterNum extends AsyncTask<Integer, Integer, ArrayAdapter>{
+		private Activity activity;
+		private ProgressDialog dialog;
+		private Context context;
+		
+		@Override
+		protected ArrayAdapter doInBackground(Integer... params) {
+			Log.d(TAG,"章数doInBackground");
+			publishProgress(params[0]);
+			String chapterQuery = "action=query_article_num&id="+tomeID;
+			
+			try{
+				int chapterNum = Integer.parseInt(Json.getRequest(HOST+chapterQuery).trim());
+				Integer[] chapterNumList = new Integer[chapterNum];
+				for(int i=0; i<chapterNum; i++){
+					chapterNumList[i] = i+1;
+				}
+				ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(Bible.this, android.R.layout.simple_list_item_1, chapterNumList);
+				return adapter;
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		public QueryChapterNum(Activity activity){
+			Log.d(TAG,"开始查询当前章数");
+			this.activity = activity;
+			this.context = activity;
+			this.dialog = new ProgressDialog(activity);
+		}
+		
+		@Override
+		protected void onPreExecute(){
+			Log.d(TAG,"章数onPreExecute");
+			this.dialog.setTitle("查询所有章数");
+			if(!this.dialog.isShowing()){
+				this.dialog.show();
+			}
+		}
+		
+		@Override
+		
+		protected void onPostExecute(ArrayAdapter adapter){
+			Log.d(TAG,"章数onPostExecute");
+			Spinner chapter = (Spinner) findViewById(R.id.chapterNum);
+			chapter.setAdapter(adapter);
+			
+			final ProgressDialog dialog = this.dialog;
+			
+			OnItemSelectedListener listener = new OnItemSelectedListener(){
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view,
+						int position, long id) {
+					chapterID = (int) Integer.parseInt(parent.getItemAtPosition(position).toString());
+					dialog.dismiss();
+					querySectionNum = (QuerySectionNum) new  QuerySectionNum(Bible.this).execute(0);
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> arg0) {
+				}};
+			chapter.setOnItemSelectedListener(listener);
+		}
+	}
+
+   /**
+    * 查询当前书卷下，当前章数下的所有节数
+    */
+	class QuerySectionNum extends AsyncTask<Integer, Integer, ArrayAdapter>{
+		private Activity activity;
+		private ProgressDialog dialog;
+		private Context context;
+		
+		public QuerySectionNum(Activity activity){
+			Log.d(TAG,"开始查询第 "+chapterID+" 章的所有节数");
+			this.activity = activity;
+			this.context = activity;
+			this.dialog = new ProgressDialog(activity);
+			this.dialog.setTitle("查询第 "+chapterID+" 章的节数");
+			if(!this.dialog.isShowing()){
+				this.dialog.show();
+			}
+		}
+		@Override
+		protected ArrayAdapter doInBackground(Integer... params) {
+			Log.d(TAG,"节数doInBackground");
+			
+			publishProgress(params[0]);
+			String sectionQuery = "action=query_verse_num&article="+chapterID+"&id="+tomeID+"";
+			try{
+				int sectionNum = Integer.parseInt(Json.getRequest(HOST+sectionQuery).trim());
+				Integer[] sectionList = new Integer[sectionNum];
+				for(int i=0; i<sectionNum; i++){
+					sectionList[i] = i+1;
+				}
+				
+				ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(Bible.this, android.R.layout.simple_list_item_1, sectionList);
+			    return adapter;
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(ArrayAdapter adapter){
+			Log.d(TAG,"节数onPostExecute");
+			
+			Spinner sectionFrom = (Spinner) findViewById(R.id.sectionFrom);
+			Spinner sectionTo = (Spinner) findViewById(R.id.sectionTo);
+			sectionFrom.setAdapter(adapter);
+			sectionTo.setAdapter(adapter);
+			
+			final ProgressDialog dialog = this.dialog;
+			
+			OnItemSelectedListener listenerTo = new OnItemSelectedListener(){
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view,
+						int position, long id) {
+					sectionToID = (int) Integer.parseInt(parent.getItemAtPosition(position).toString());
+					dialog.dismiss();
+					queryBibleDetail = (QueryBibleDetail) new QueryBibleDetail(Bible.this).execute(0);
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> arg0) {
+				}};
+			OnItemSelectedListener listenerFrom = new OnItemSelectedListener(){
+					@Override
+					public void onItemSelected(AdapterView<?> parent, View view,
+							int position, long id) {
+						sectionFromID = (int) Integer.parseInt(parent.getItemAtPosition(position).toString());
+						dialog.dismiss();
+						queryBibleDetail = (QueryBibleDetail) new QueryBibleDetail(Bible.this).execute(0);
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> arg0) {
+					}};
+			sectionFrom.setOnItemSelectedListener(listenerFrom);
+			sectionTo.setOnItemSelectedListener(listenerTo);
+		}
+	}
+   
+	/**
+	 * 根据书卷，章数，节数查询具体的经文
+	 */
+	class QueryBibleDetail extends AsyncTask<Integer, Integer, String>{
+		private Activity activity;
+		private ProgressDialog dialog;
+		private Context context;
+		
+		public QueryBibleDetail(Activity activity){
+			this.activity = activity;
+			this.context = activity;
+			this.dialog = new ProgressDialog(activity);
+			this.dialog.setTitle("查询经文");
+			this.dialog.setMessage("正在查询："+tome+chapterID+":"+sectionFromID+"-"+sectionToID);
+			if(!this.dialog.isShowing()){
+				this.dialog.show();
+			}
+		}
+		
+		@Override
+		protected String doInBackground(Integer... params) {
+			Log.d(TAG,"经文doInBackground");
+			publishProgress(params[0]);
+			
+			if(sectionFromID > sectionToID){
+				return "";
+			}
+			
+			String queryBible = "action=query_bible&article="+chapterID+"&id="+tomeID+"&verse_start="+sectionFromID+"&verse_stop="+sectionToID+"";
+			try{
+				String bible = (Json.getRequest(HOST+queryBible)).trim();
+				bible = android.text.Html.fromHtml(bible).toString();
+				return bible;
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(String bible){
+			Log.d(TAG,"经文onPostExecute");
+			TextView bibleBox = (TextView) findViewById(R.id.bibleBox);
+			bibleBox.setText(bible);
+			this.dialog.dismiss();
 		}
 	}
 }
