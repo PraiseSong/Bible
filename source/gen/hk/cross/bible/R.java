9c7dd871068697157f3a34fb432b682aafe03bd2
praise <qiqiyoujian@gmail.com>
2013-02-25 20:04:15 +0800

    更新logo

--- source/gen/hk/cross/bible/R.java
+++ source/gen/hk/cross/bible/R.java
@@ -22,7 +22,7 @@ public final class R {
         public static final int droplist_default=0x7f020001;
         public static final int droplist_down=0x7f020002;
         public static final int droplist_focus=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int logo=0x7f020004;
     }
     public static final class id {
         public static final int bibleBox=0x7f08000d;
